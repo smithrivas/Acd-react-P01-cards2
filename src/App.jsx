8c133fd7984daa9acbbcc69bda6517196c940fb2
Brayan <smithrivas.dev@gmail.com>
2023-02-28 21:54:27 -0500

    Actualización ruta imagenes, se agregaron online

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import Button from "./components/Button";
 
 function App() {
   const themes = [
-    ["image1.jpg", "rgb(18, 20, 53)"],
-    ["image2.jpg", "rgb(85, 44, 15)"],
-    ["image3.jpg", "rgb(27, 22, 67)"],
-    ["image4.jpg", "rgb(115, 16, 16)"],
+    ["bES6nmb.jpeg", "rgb(18, 20, 53)"],
+    ["CnkwK3h.jpeg", "rgb(85, 44, 15)"],
+    ["uTdf5Ym.jpeg", "rgb(27, 22, 67)"],
+    ["O0BORVQ.jpeg", "rgb(115, 16, 16)"],
   ];
   const [index, setIndex] = useState(Math.floor(Math.random() * phrase.length));
   const [indexTheme, setIndexTheme] = useState(
@@ -18,14 +18,14 @@ function App() {
 
   let themeSel = themes[Number(indexTheme)][0];
   let colorSel = themes[Number(indexTheme)][1];
-  document.body.style = `background-image: url(public/img/${themeSel}); color: ${colorSel}`;
+  document.body.style = `background-image: url(https://i.imgur.com/${themeSel}); color: ${colorSel}`;
 
   function changePhrase() {
     setIndex(Math.floor(Math.random() * phrase.length));
     setIndexTheme(Math.floor(Math.random() * themes.length));
     let themeSel = themes[Number(indexTheme)][0];
     let colorSel = themes[Number(indexTheme)][1];
-    document.body.style = `background-image: url(proyecto1-frases/public/img/${themeSel}); color: ${colorSel}`;
+    document.body.style = `background-image: url(https://i.imgur.com/${themeSel}); color: ${colorSel}`;
   }
 
   return (
